87d75bf25d63f5fe800b9551813c6822089927ac
Altmerian <altmersc@gmail.com>
2025-06-07 19:23:24 +0200

    feat: add Prisma schema and initial database migration

---
+++ prisma/migrations/20250607170950_init/migration.sql
@@ -0,0 +1,54 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "login" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "version" INTEGER NOT NULL,
+    "createdAt" BIGINT NOT NULL,
+    "updatedAt" BIGINT NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "artists" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "grammy" BOOLEAN NOT NULL,
+
+    CONSTRAINT "artists_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "albums" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "year" INTEGER NOT NULL,
+    "artistId" TEXT,
+
+    CONSTRAINT "albums_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "tracks" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "duration" INTEGER NOT NULL,
+    "artistId" TEXT,
+    "albumId" TEXT,
+
+    CONSTRAINT "tracks_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "favorites" (
+    "id" TEXT NOT NULL DEFAULT 'singleton',
+    "artists" TEXT[],
+    "albums" TEXT[],
+    "tracks" TEXT[],
+
+    CONSTRAINT "favorites_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_login_key" ON "users"("login");
